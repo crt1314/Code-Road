0855afabbfcd820e6073c4979134bf2f5bef9131
crt1314 <3378310437@qq.com>
2022-11-30 22:14:10 +0800

    目录页面待完善

--- static/js/common.js
+++ static/js/common.js
@@ -8,16 +8,19 @@
 function changeTitleWhenUserLeaveAndArrive() {
     // 保存文档原来的title
     const origin_title = document.title;
+    // 保存timeout返回的id
+    let timeoutID;
 
     // 页面失去焦点
     window.onblur = function () {
+        if (timeoutID !== undefined) clearTimeout(timeoutID);
         document.title = "宝，别走(ಥ﹏ಥ)";
     };
 
     // 页面获取焦点
     window.onfocus = function () {
         document.title = "宝，欢迎回来o((*^▽^*))o";
-        setTimeout(changeToOriginTitle, 1000);
+        timeoutID = setTimeout(changeToOriginTitle, 1000);
     };
 
     // 将页面title改为原来值
@@ -39,4 +42,40 @@ function createSpanElement(content, cls) {
     span.textContent = content;
     if (cls !== undefined) span.classList.add(cls);
     return span;
+}
+
+/**
+ * 显示加载动画
+ * @author crt1314
+ * @version 1.0.0
+ */
+function loading() {
+    const loadingDiv = document.createElement("div");
+    const loading = document.createElement("div");
+    const content = document.createElement("h2");
+    loadingDiv.id = "loadingDiv";
+    content.textContent = "Loading...";
+    for (let i = 0; i < 4; i++) {
+        loading.appendChild(createSpanElement(""));
+    }
+    loading.appendChild(content);
+    loadingDiv.appendChild(loading);
+    document.body.appendChild(loadingDiv);
+}
+
+/**
+ * 检查header背景图片加载
+ * @author crt1314
+ * @version 1.0.0
+ */
+function backgroundImageLoad() {
+    const header = document.querySelector("header[role='banner']");
+    const src = getComputedStyle(header).background.match(/url\(\"?(.*)\"\)/)[1];
+    const img = new Image();
+    img.src = src;
+    img.onload = function () {
+        document.body.style.hidden = "auto";
+        document.body.removeChild(document.getElementById("loadingDiv"));
+        header.style.transform = "translateY(0)";
+    }
 }
--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,9 @@
 'use script';
 
-/**
- * 页面加载
- */
 window.onload = function () {
     changeTitleWhenUserLeaveAndArrive();
     showWelcome();
+    blessings_show_and_hidden();
 };
 
 /**
@@ -21,8 +19,6 @@ function showWelcome() {
     header.role = 'banner';
     welcomeDiv.id = "welcome";
     h1.textContent = "Code Road";
-    h2.appendChild(document.createTextNode("Welcome to "));
-    h2.appendChild(createSpanElement("Code Road"));
     welcomeDiv.appendChild(h1);
     welcomeDiv.appendChild(h2);
     welcomeDiv.appendChild(description());
@@ -38,6 +34,10 @@ function showWelcome() {
  */
 function description() {
     const desc = document.createElement("p");
+    const link = document.createElement("a");
+    link.title = "go!!!!!";
+    link.href = "study/menu.html";
+    link.textContent = "( ๑‾̀◡‾́)σ»";
     desc.appendChild(document.createTextNode(
         "This website is made for recording learning trails " +
         "and will document content on "));
@@ -54,6 +54,54 @@ function description() {
     desc.appendChild(createSpanElement(
         "all this can not be achieved overnight and success relies on perseverance. ",
         "highlight"));
-    desc.appendChild(document.createTextNode("Let we go, ( ๑‾̀◡‾́)σ»"));
+    desc.appendChild(document.createTextNode("Let we go, "));
+    desc.appendChild(link);
     return desc;
+}
+
+/**
+ * 展示与隐藏祝福语
+ * @author crt1314
+ * @version 1.0.0
+ */
+function blessings_show_and_hidden() {
+    const blessings = [
+        "There is no royal road to learning",
+        "I'm not proud of everything I did, but I'm pretty sure I'd do it all again",
+        "It's so easy to be careless, it takes courage and courage to care",
+        "You're not a nobody, you are somebody",
+        "Doubt is the key to knowledge",
+        "I used to think that my life was a tragedy. But now I realize, it's a comedy",
+        "Life was like a box of chocolate ,you never know what you're gonna get",
+        "Hope is a good thing and maybe the best of things. And no good thing ever dies",
+        "The greatest test of courage on earth is to bear defeat without losing heart",
+        "A man's best friends are his ten fingers",
+        "The shortest way to do many things is to only one thing at a time",
+        "Sow nothing, reap nothing",
+        "Life is a horse, and either you ride it or it rides you"
+    ];
+    const h2 = document.querySelector("#welcome h2");
+    let i = 0, blessingsWordLength = 0;
+    let intervalID = setInterval(show_blessings, 200);
+
+    function show_blessings() {
+        h2.textContent = blessings[i].substr(0, blessingsWordLength++);
+        if (blessingsWordLength > blessings[i].length) {
+            clearInterval(intervalID);
+            setTimeout(pause, 500);
+        }
+    }
+
+    function hidden_blessings() {
+        h2.textContent = blessings[i].substr(0, --blessingsWordLength);
+        if (!blessingsWordLength) {
+            clearInterval(intervalID);
+            intervalID = setInterval(show_blessings, 200);
+            i = (i + 1) % blessings.length;
+        }
+    }
+
+    function pause() {
+         intervalID = setInterval(hidden_blessings, 100);
+    }
 }
